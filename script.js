7d6cb8ff21d3057079040cf16cd3134aa01add4b
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-04-08 15:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+	this.name = name;
+		
+	}
+	get name()
+	{
+		return this.name;
+	}
 
-class Student extends Person {}
+	set age()
+	{
+		this.age = age;
+	}
+	
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+
+	study()
+	{
+		console.log(this.name+" is studying");
+	}
+}
+
+class Teacher extends Person {
+
+	teach(){
+		console.log(this.name+" is teaching");
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
